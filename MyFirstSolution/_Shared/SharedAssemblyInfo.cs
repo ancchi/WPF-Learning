292c9444e7b20b5246e0d375694407ae92c4d58d
Ancchi <antje1.fischer@yahoo.de>
2021-03-30 13:22:39 +0200

    AssemlbyInfo.cs cannot be linked into Properties folder

--- MyFirstSolution/_Shared/SharedAssemblyInfo.cs
+++ MyFirstSolution/_Shared/SharedAssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MeineLeereProjektMappe")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyTrademark("MeineLeereProjektMappe")]
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Logic.Core")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Logic.Core")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
