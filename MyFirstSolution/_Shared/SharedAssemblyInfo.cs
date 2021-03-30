fb012998b7873a4eb2f00b06e4f2cc1a43ccd781
Ancchi <antje1.fischer@yahoo.de>
2021-03-30 12:55:42 +0200

    Selfbuild solution with SharedAssemblyInfo.cs

---
+++ MyFirstSolution/_Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MeineLeereProjektMappe")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("MeineLeereProjektMappe")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
