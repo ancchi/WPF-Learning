ec1a66be174d7d189a4477bde532c803af2c9e57
Ancchi <antje1.fischer@yahoo.de>
2021-03-30 16:30:30 +0200

    SharedAssemblyInfo could not be used with WPF Application

--- MyFirstSolution/_Shared/SharedAssemblyInfo.cs
+++ MyFirstSolution/_Shared/SharedAssemblyInfo.cs
@@ -1,5 +1,8 @@
 using System.Reflection;
 
+// SharedAssemblyInfo.cs
+// Solution selbst gebaut nach https://codingfreaks.de/solution-bauen/
+
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logic.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
