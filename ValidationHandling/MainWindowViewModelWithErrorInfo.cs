37f15c2f0ffeefa632c85ad98f899a363a79885c
Torsten Zimmermann <zimmy@web.de>
2021-04-08 11:04:11 +0200

    Gültigkeitsüberprüfung

---
+++ ValidationHandling/MainWindowViewModelWithErrorInfo.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationHandling
+{
+    public class MainWindowViewModelWithErrorInfo : INotifyPropertyChanged, IDataErrorInfo
+    {
+        public MainWindowViewModelWithErrorInfo()
+        {
+            firstname = "Hans-Peter";
+            lastname = "Schmidt";
+            city = "Berlin";
+            Street.Name = "Alexanderplatz";
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private string firstname;
+
+        public string FirstName
+        {
+            get => firstname;
+            set
+            {
+                if (!string.Equals(firstname, value))
+                {
+                    firstname = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string lastname;
+
+        public string LastName
+        {
+            get => lastname;
+            set
+            {
+                if (!string.Equals(lastname, value))
+                {
+                    lastname = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+
+        private string city;
+
+        public string City
+        {
+            get => city;
+            set
+            {
+                if (!string.Equals(city, value))
+                {
+                    city = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private Street street;
+
+        public Street Street
+        {
+            get => street ??= new StreetWithNotification();
+            set
+            {
+                if (street != value)
+                {
+                    street = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string Error => string.Empty;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName.Equals(nameof(LastName)) &&
+                    string.IsNullOrEmpty(LastName))
+                {
+                    return "The LastName should not be empty";
+                }
+
+                if (columnName.Equals(nameof(City)) &&
+                    string.IsNullOrEmpty(City))
+                {
+                    return "The City should not be empty";
+                }
+
+                return String.Empty;
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ ValidationHandling/Street.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationHandling
+{
+    public class Street
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string Number { get; set; }
+    }
+
+
+    public class StreetWithNotification : Street, INotifyPropertyChanged, IDataErrorInfo
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override string Name
+        {
+            get => base.Name;
+            set
+            {
+                if (!string.Equals(base.Name, value))
+                {
+                    base.Name = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public override string Number
+        {
+            get => base.Number;
+            set
+            {
+                if (!string.Equals(base.Number, value))
+                {
+                    base.Number = value;
+                    RaisePropertyChanged();
+                }
+            } 
+        }
+
+        public string Error => "";
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName.Equals(nameof(Name)) && string.IsNullOrEmpty(Name))
+                    return "The name of the street should not be empty";
+
+                return null;
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+
+}
