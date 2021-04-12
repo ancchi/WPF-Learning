7eedc73dcf8984466416c03b5e97584b978a5626
Ancchi <antje1.fischer@yahoo.de>
2021-04-12 12:25:27 +0200

    BooleanToColorConverter umbenannt nach BooeanToColorIValueConverter

--- Block1Uebung3/Converters/BooleanToColorIValueConverter.cs
+++ Block1Uebung3/Converters/BooleanToColorIValueConverter.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
 
 namespace Block1Uebung3.Converters {
-    class BooleanToColorConverter : IValueConverter {
+    class BooleanToColorIValueConverter : IValueConverter {
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
 
