144bfbb8cb1e5472f591b9d8f522f3fa1a2c88dd
Ancchi <antje1.fischer@yahoo.de>
2021-04-14 15:56:26 +0200

    Converter überarbeitet

--- Block2Aufgaben2Bis4OhnePrism/Converter/MyStudentTypeConverter.cs
+++ Block2Aufgaben2Bis4OhnePrism/Converter/MyStudentTypeConverter.cs
@@ -21,18 +21,38 @@ namespace Block2Aufgaben2Bis4OhnePrism.Converter {
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value) {
 
             if (value is string) {
-
-                string[] studentArray = value.ToString().Split(", ");
+                string[] seperator = { ", ", ","};
+                string[] studentArray = value.ToString().Split(seperator, StringSplitOptions.None);
+                bool arrayElementsNotEmpty = false;
                
-
-                if (studentArray.Length == 3) {
-                    bool isBoolean = bool.TryParse(studentArray[2], out isBoolean);
-                    if (isBoolean) {
-                        return new Student { PreName = studentArray[1], LastName = studentArray[0], IstAnwesend = Convert.ToBoolean(studentArray[2]) };
+                // sind die Elemente des Arrays alle gefüllt?
+                for (int i = 0; i < studentArray.Length; i++) {
+                    if (studentArray[i].Trim().Length > 0) {
+                        arrayElementsNotEmpty = true;
+                    } else {
+                        arrayElementsNotEmpty = false;
+                        break;
                     }
-                    // dass der erste Eintrag ein Name und der zweite Eintrag ein Boolean ist, wird durch einen Validator sichergestellt
-                } else if (studentArray.Length == 2) {
-                    return new Student { PreName = studentArray[1], LastName = studentArray[0], IstAnwesend = false };
+                }
+
+                if (arrayElementsNotEmpty) { // wenn kein Element leer ist, kann man sie zählen
+                    if (studentArray.Length == 3) {
+                       
+                        bool istAnwesend;
+                        if (studentArray[2].ToLower().Equals("ja")) {
+                            istAnwesend = true;
+                        } else if (studentArray[2].ToLower().Equals("nein")) {
+                            istAnwesend = false;
+                        } else {
+                            return null;
+                        }
+
+                        return new Student { PreName = studentArray[1], LastName = studentArray[0], IstAnwesend = istAnwesend };
+
+                      // nur Vor- und Nachname wurden eingegeben
+                    } else if (studentArray.Length == 2) {
+                        return new Student { PreName = studentArray[1], LastName = studentArray[0], IstAnwesend = false };
+                    }
                 }
             }
             //return base.ConvertFrom(context, culture, value);
