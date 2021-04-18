5f9ca9e098b62bd1e10b9b731c9c388dbfbaa0b3
Ancchi <antje1.fischer@yahoo.de>
2021-04-18 14:08:45 +0200

    Images, Listbox mit Images, einzelnen Eintrag in Listbox visualisiert

---
+++ MyLayoutPlayground/Models/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLayoutPlayground.Models {
+    class Student : INotifyPropertyChanged {
+        private string preName;
+        private string lastName;
+        private string imgPath;
+
+        public string PreName {
+            get => preName;
+            set {
+                if (!string.Equals(preName, value)) {
+                    preName = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string LastName {
+            get => lastName;
+            set {
+                if(!string.Equals(lastName, value)) {
+                    lastName = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string ImgPath {
+            get => imgPath;
+            set {
+                if(!string.Equals(imgPath, value)) {
+                    imgPath = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+    }
+}
---
+++ MyLayoutPlayground/ViewModels/StudentViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MyLayoutPlayground.Models;
+
+namespace MyLayoutPlayground.ViewModels {
+    class StudentViewModel : INotifyPropertyChanged {
+
+        public StudentViewModel() {
+            generateStudentList();
+        }
+
+        private Student student;
+
+        private Student Student {
+            get => student ??= new Student();
+            set {
+                if(!string.Equals(student, value)) {
+                    student = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private ObservableCollection<Student> studentList = new();
+
+        public ObservableCollection<Student> StudentList {
+            get => studentList;
+            set {
+                if (studentList != value) {
+                    studentList = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        string path = "/images/";
+
+        public void generateStudentList() {
+            studentList.Add(new Student { PreName = "Lissy", LastName = "Baumann", ImgPath = path + "Baer.jpg" });
+            studentList.Add(new Student { PreName = "Mark", LastName = "Ficus", ImgPath = path + "Hund.jpg" });
+            studentList.Add(new Student { PreName = "Alex", LastName = "Sander", ImgPath = path + "Panda.jpg" });
+            studentList.Add(new Student { PreName = "Clara", LastName = "See", ImgPath = path + "Schaf.jpg" });
+            studentList.Add(new Student { PreName = "Sibirius", LastName = "Waystine", ImgPath = path + "Ziege.jpg" });
+
+            StudentList = studentList;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
